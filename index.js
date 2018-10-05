7835d40823954c98f16f09ab7e4793061cea5ca2
Hidenori Fujimura <hfu@users.noreply.github.com>
2018-10-04 21:16:32 -0400

    update

---
+++ index.js
@@ -0,0 +1,24 @@
+const config = require('config')
+const zlib = require('zlib')
+const Database = require('better-sqlite3')
+const VectorTile = require('@mapbox/vector-tile').VectorTile
+const Pbf = require('pbf')
+
+const db = Database(config.get('mbtiles'), {readonly: true})
+
+const stmt = db.prepare('SELECT * FROM tiles')
+for (const row of stmt.iterate()) {
+  let z, x, y, tile
+  [z, x, y] = [row.zoom_level, row.tile_column, row.tile_row]
+  tile = new VectorTile(new Pbf(zlib.unzipSync(row.tile_data)))
+  for (let layerName of Object.keys(tile.layers)) {
+    let layer = tile.layers[layerName]
+    for (let i = 0; i < layer.length; i++) {
+      let f = layer.feature(i).toGeoJSON(x, y, z)
+      f.tippecanoe = { layer: layerName }
+      console.log(JSON.stringify(f))
+    }
+  }
+}
+
+db.close
